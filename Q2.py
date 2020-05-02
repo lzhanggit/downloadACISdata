995da566d85c6d7015084fe8a4cb4f376c542898
lzhanggit <zlg@huskers.unl.edu>
2020-05-02 14:37:28 -0500

    update Q2

--- Q2.py
+++ Q2.py
@@ -1,15 +1,15 @@
-""" 
-		This function was coded and modified by Ligang Zhang for class project
-
-		May 1, 2020
+# -*- coding: utf-8 -*-
 """
+Created on Sat May  2 11:21:11 2020
+
+@author: LZHome
+"""
+
+
 import pandas as pd
 import numpy as np
 from ACISdownloader import downloader 
 format1=lambda x:"%.1f"%x
-#Question 1
-p1 = downloader(sids="KCAR",sdate='2019-1-1',edate='2019-12-31',elems="avgt")
-p1.to_csv('Q1_test_multiday_KCAR_station.csv',index=False,header=True)
 
 #Question 2
 p2 = downloader(county="31109",sdate='2019-1-1',edate='2019-12-31',elems="avgt")
@@ -93,23 +93,3 @@ dfcounty.columns = ['date','station name','Mean Temperature (F)']
 print(dfcounty)
 
 dfcounty.to_csv('Q2_mutistation5Daymean.csv',index=False,header=True)
-
-#Question 3
-p3 = downloader(county="31109",sdate='2019-1-1',edate='2019-12-31',elems="avgt")
-p3.head()
-df3 = pd.DataFrame(p3,columns=['date','uid','avgt'])
-print(df3)
-df3.dtypes
-df3["avgt"] = df3.avgt.astype(float)
-df3.dtypes
-#date format
-df3['date']=pd.to_datetime(df3['date'],format='%Y%m%d')
-#date index
-df3 = df3.set_index('date')
-#monthly mean
-dfM=df3.resample('M').mean()
-dfM[['avgt']]=dfM[['avgt']].applymap(format1)
-dfM['uid']=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-dfM.columns = ['Month','Mean Temperature (F)']
-print(dfM)
-dfM.to_csv('Q3_test_temperature_monthly_mean_Lancaster.csv',index=True,header=True)
